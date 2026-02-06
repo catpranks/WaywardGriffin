de318e5b1b4c56204c2a0392210924549a6acd34
Cat Pranks <pranks@catpranks.tv>
2026-02-06 00:39:11 -0800

    screencopy buffer

--- src/capture/source/wlr_screencopy.rs
+++ src/capture/source/wlr_screencopy.rs
@@ -3,6 +3,7 @@ use crate::capture::input::InputInjector;
 use crate::capture::input::dummy::DummyInput;
 use crate::capture::plotter::PlotterHandle;
 use anyhow::{Context as _, Result, bail};
+use drm_fourcc::DrmFourcc;
 use smithay_client_toolkit::dmabuf::{DmabufFeedback, DmabufHandler, DmabufState};
 use smithay_client_toolkit::output::{OutputHandler, OutputState};
 use smithay_client_toolkit::reexports::calloop::EventLoop;
@@ -15,15 +16,23 @@ use smithay_client_toolkit::registry::{ProvidesRegistryState, RegistryState};
 use smithay_client_toolkit::{
     delegate_dmabuf, delegate_output, delegate_registry, registry_handlers,
 };
+use std::os::fd::AsFd as _;
 use std::os::unix::net::UnixStream;
 use std::sync::Arc;
 use vulkano::device::Device;
-use vulkano::memory::allocator::StandardMemoryAllocator;
+use vulkano::format::Format;
+use vulkano::image::sys::RawImage;
+use vulkano::image::{ImageAspect, ImageCreateInfo, ImageTiling, ImageUsage};
+use vulkano::memory::allocator::{MemoryAllocator as _, MemoryTypeFilter, StandardMemoryAllocator};
+use vulkano::memory::{
+    DedicatedAllocation, DeviceMemory, ExternalMemoryHandleType, ExternalMemoryHandleTypes,
+    MemoryAllocateInfo, ResourceMemory,
+};
 use vulkano::sync::fence::Fence;
 
 // breaks rustfmt import sorting for some reason
 use smithay_client_toolkit::reexports::protocols::wp::linux_dmabuf::zv1::client::zwp_linux_dmabuf_feedback_v1::ZwpLinuxDmabufFeedbackV1;
-use smithay_client_toolkit::reexports::protocols::wp::linux_dmabuf::zv1::client::zwp_linux_buffer_params_v1::ZwpLinuxBufferParamsV1;
+use smithay_client_toolkit::reexports::protocols::wp::linux_dmabuf::zv1::client::zwp_linux_buffer_params_v1::{self, ZwpLinuxBufferParamsV1};
 use smithay_client_toolkit::reexports::protocols_wlr::screencopy::v1::client::zwlr_screencopy_frame_v1::{self, ZwlrScreencopyFrameV1};
 use smithay_client_toolkit::reexports::protocols_wlr::screencopy::v1::client::zwlr_screencopy_manager_v1::{self, ZwlrScreencopyManagerV1};
 
@@ -41,7 +50,8 @@ struct State {
 impl State {
     fn start_capture(&self) {
         let output = self.output.as_ref().unwrap();
-        self.screencopy_manager.capture_output(1, output, &self.qh, ());
+        self.screencopy_manager
+            .capture_output(1, output, &self.qh, ());
     }
 }
 
@@ -226,6 +236,101 @@ impl CaptureBackendBuilder for Builder {
     }
 }
 
+struct PooledBuffer {
+    wl_buffer: WlBuffer,
+    image: Arc<vulkano::image::Image>,
+    fence: Option<Arc<Fence>>,
+}
+
+impl PooledBuffer {
+    fn new(
+        device: &Arc<Device>,
+        allocator: &StandardMemoryAllocator,
+        dmabuf_state: &DmabufState,
+        qh: &QueueHandle<State>,
+        format: u32,
+        width: u32,
+        height: u32,
+    ) -> Result<Self> {
+        let vk_format = fourcc_to_format(format)?;
+
+        let raw_image = RawImage::new(
+            device.clone(),
+            ImageCreateInfo {
+                format: vk_format,
+                extent: [width, height, 1],
+                usage: ImageUsage::TRANSFER_SRC | ImageUsage::SAMPLED,
+                external_memory_handle_types: ExternalMemoryHandleTypes::DMA_BUF,
+                tiling: ImageTiling::Linear,
+                ..Default::default()
+            },
+        )?;
+
+        let req = raw_image.memory_requirements()[0];
+        let alloc = DeviceMemory::allocate(
+            device.clone(),
+            MemoryAllocateInfo {
+                allocation_size: req.layout.size(),
+                memory_type_index: allocator
+                    .find_memory_type_index(req.memory_type_bits, MemoryTypeFilter::PREFER_DEVICE)
+                    .context("no suitable memory type")?,
+                dedicated_allocation: Some(DedicatedAllocation::Image(&raw_image)),
+                export_handle_types: ExternalMemoryHandleTypes::DMA_BUF,
+                ..Default::default()
+            },
+        )?;
+
+        let fd = alloc.export_fd(ExternalMemoryHandleType::DmaBuf)?;
+        let layout = raw_image.subresource_layout(ImageAspect::Color, 0, 0)?;
+
+        let image = Arc::new(
+            raw_image
+                .bind_memory([ResourceMemory::new_dedicated(alloc)])
+                .map_err(|(e, _, _)| e)?,
+        );
+
+        let params = dmabuf_state.create_params(qh)?;
+        params.add(fd.as_fd(), 0, 0, layout.row_pitch as u32, 0);
+        let (wl_buffer, _params) = params.create_immed(
+            width as i32,
+            height as i32,
+            format,
+            zwp_linux_buffer_params_v1::Flags::empty(),
+            qh,
+        );
+
+        Ok(Self {
+            wl_buffer,
+            image,
+            fence: None,
+        })
+    }
+}
+
+fn fourcc_to_format(fourcc: u32) -> Result<Format> {
+    match DrmFourcc::try_from(fourcc).context("unknown fourcc")? {
+        DrmFourcc::Argb8888 | DrmFourcc::Xrgb8888 => Ok(Format::B8G8R8A8_SRGB),
+        DrmFourcc::Abgr8888 | DrmFourcc::Xbgr8888 => Ok(Format::R8G8B8A8_SRGB),
+        other => bail!("unsupported fourcc: {other:?}"),
+    }
+}
+
+impl Drop for PooledBuffer {
+    fn drop(&mut self) {
+        self.wl_buffer.destroy();
+    }
+}
+
+struct BufferHandle {
+    wl_buffer: WlBuffer,
+}
+
+impl Drop for BufferHandle {
+    fn drop(&mut self) {
+        self.wl_buffer.destroy();
+    }
+}
+
 struct Backend;
 
 impl CaptureBackend for Backend {
@@ -233,8 +338,7 @@ impl CaptureBackend for Backend {
         Ok(None)
     }
 
-    fn release(&mut self, _frame: CapturedFrame, _fence: Option<Arc<Fence>>) {
-    }
+    fn release(&mut self, _frame: CapturedFrame, _fence: Option<Arc<Fence>>) {}
 }
 
 fn event_loop_thread(conn: Connection, event_queue: EventQueue<State>, mut state: State) {
